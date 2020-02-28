7b843c30c36aaf1fdd4a9ec83bae72841e83c41e
Sandeep Sabu <ssabu3@asurite.asu.edu>
2020-02-27 18:02:18 -0700

    second commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 !async function(){
 
-  var myMap = L.map('mapid').setView([33.2918, -112.4291], 8);
+  var myMap = L.map('mapid').setView([33.2918, -112.4291], 8.5);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -17,7 +17,15 @@
    .then(res => res.json())
    .then(function(geoJson) { return geoJson; });
 
+  slider();
+
   let gja = geoJsonConversion(dataObj);
+
+  // console.log(gja);
+
+  var ptLyr96 = gja[0][1996];
+
+  // L.geoJSON(ptLyr96).addTo(myMap);
 
   var ctCount = pointCount(geoJson, gja);
 
@@ -36,6 +44,8 @@
     ctJsonObj.push(yearLyrObj);
   });
 
+  // console.log("this is ct object", ctJsonObj);
+
   function slider() {
 
     var dataTime = [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019];
@@ -50,6 +60,8 @@
       .tickValues(dataTime)
       .on('onchange', val => {
         d3.select('p#value-time').text(val);
+
+        // console.log(sliderTime.value());
 
         gj96.remove();
 
@@ -97,13 +109,159 @@
 
     d3.select('p#value-time').text(sliderTime.value());
 
+    var b = d3.select("#slider-time");
+
+    // console.log(typeof(sliderTime.value()));
+
+    // sliderTime.value(2008);
+
+    var myTimer;
+
+    var year = 1996;
+
+    d3.select("#play").on("click", function() {
+      // clearInterval(myTimer);
+      myTimer = setInterval(function() {
+        // console.log(year);
+        sliderTime.value(++year);
+        if (year === 2020) {
+          clearInterval(myTimer);
+        }
+      }, 1000);
+    });
+
+    d3.select("#stop").on("click", function() {
+      clearInterval(myTimer);
+    });
+
+    d3.select("#reload").on("click", function() {
+
+      var currYear = sliderTime.value();
+
+      // console.log("this is current year", currYear);
+
+      // console.log(ctJsonObj[currYear-1996][currYear]);
+
+      var currYrLyr = ctJsonObj[currYear-1996][currYear];
+
+      currYrLyr.removeFrom(myMap);
+
+      sliderTime.value(1996);
+    });
+
   };
 
-  slider();
 
-  function play() {
+
+  // function play() {
+  //
+  // };
+
+  function legend() {
+
+    var svg = d3.select(myMap.getPanes().overlayPane).append("svg").attr("width", 2000).attr("height", 500).attr('transform', 'translate(1300,340)');
+
+    svg.append("circle")
+       .attr("cx", 100)
+       .attr("cy", 20)
+       .attr("r", 5)
+       .attr("stroke", "black")
+       .attr("fill", "#800026");
+
+    svg.append("text")
+       .attr("x", 110)
+       .attr("y", 22)
+       .text(">= 100")
+       .style("font-size", "13px")
+       .attr("alignment-baseline", "middle");
+
+     svg.append("circle")
+        .attr("cx", 100)
+        .attr("cy", 40)
+        .attr("r", 5)
+        .attr("stroke", "black")
+        .attr("fill", "#BD0026");
+
+    svg.append("text")
+       .attr("x", 110)
+       .attr("y", 42)
+       .text(">= 75")
+       .style("font-size", "13px")
+       .attr("alignment-baseline", "middle");
+
+    svg.append("circle")
+       .attr("cx", 100)
+       .attr("cy", 60)
+       .attr("r", 5)
+       .attr("stroke", "black")
+       .attr("fill", "#E31A1C");
+
+   svg.append("text")
+      .attr("x", 110)
+      .attr("y", 62)
+      .text(">= 50")
+      .style("font-size", "13px")
+      .attr("alignment-baseline", "middle");
+
+     svg.append("circle")
+        .attr("cx", 100)
+        .attr("cy", 80)
+        .attr("r", 5)
+        .attr("stroke", "black")
+        .attr("fill", "#FC4E2A");
+
+    svg.append("text")
+       .attr("x", 110)
+       .attr("y", 82)
+       .text(">= 35")
+       .style("font-size", "13px")
+       .attr("alignment-baseline", "middle");
+
+      svg.append("circle")
+         .attr("cx", 100)
+         .attr("cy", 100)
+         .attr("r", 5)
+         .attr("stroke", "black")
+         .attr("fill", "#FD8D3C");
+
+     svg.append("text")
+        .attr("x", 110)
+        .attr("y", 102)
+        .text(">= 10")
+        .style("font-size", "13px")
+        .attr("alignment-baseline", "middle");
+
+       svg.append("circle")
+          .attr("cx", 100)
+          .attr("cy", 120)
+          .attr("r", 5)
+          .attr("stroke", "black")
+          .attr("fill", "#FEB24C");
+
+      svg.append("text")
+         .attr("x", 110)
+         .attr("y", 122)
+         .text("> 0")
+         .style("font-size", "13px")
+         .attr("alignment-baseline", "middle");
+
+        svg.append("circle")
+           .attr("cx", 100)
+           .attr("cy", 140)
+           .attr("r", 5)
+           .attr("stroke", "black")
+           .attr("fill", "#FFF");
+
+       svg.append("text")
+          .attr("x", 110)
+          .attr("y", 142)
+          .text("0")
+          .style("font-size", "13px")
+          .attr("alignment-baseline", "middle");
 
   };
+
+  legend();
 
   function pointCount(polyGeoJson, pointGeoJsonArray) {
     var polyGeoJsonCount = [];
@@ -128,7 +286,8 @@
     };
 
   function showData(data) {
-    const years = [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2019];
+    // console.log(data);
+    const years = [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019];
     const newDataObj = [];
     years.forEach(function(year) {
       var currYear = year;
@@ -136,6 +295,7 @@
       var yearObj = {};
       data.forEach(function(el) {
         var dataYear = parseInt(el.saledate.slice(0,4));
+        // console.log(dataYear, currYear);
         if (dataYear === currYear) {
           currData.push([el.long, el.lat]);
         }
@@ -143,6 +303,7 @@
       yearObj[year] = currData;
       newDataObj.push(yearObj);
     });
+    // console.log("new data object", newDataObj);
     return newDataObj;
   };
 
@@ -172,13 +333,13 @@
         geoJsonArray.push(yearObj);
         });
       });
-      var yearObj18 = {};
-      var fc18 = {
-        "type": "FeatureCollection",
-        "features": []
-      }
-      yearObj18[2018] = fc18;
-      geoJsonArray.splice(geoJsonArray.length-1,0,yearObj18);
+      // var yearObj18 = {};
+      // var fc18 = {
+      //   "type": "FeatureCollection",
+      //   "features": []
+      // }
+      // yearObj18[2018] = fc18;
+      // geoJsonArray.splice(geoJsonArray.length-1,0,yearObj18);
       // console.log(geoJsonArray);
       return geoJsonArray;
   };
